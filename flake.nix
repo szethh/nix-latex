54e46d9b68a82fbe600349f51c1aa08ae1014463
Clara Periago <clara.periagoserrano@ru.nl>
2025-02-04 15:03:46 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "LaTeX development environment with VS Code";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    {
+      devShellFor =
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            texliveMedium
+            tectonic # compiler
+            ltex-ls # Grammar/spelling checker for LaTeX
+            aspell
+            aspellDicts.en
+            #   (pkgs.vscode-extensions.james-yu.latex-workshop)
+          ];
+        };
+    };
+}
